07cfa2419eabd9bce991da5c4a9f91f793eb4a20
bdring <barton.dring@gmail.com>
2019-04-24 14:33:58 -0500

    Updated RMT config to be more Grbl like
     - Added RMT channels numbers to cpu_map.h
     - This allows same config flexibility as master
    - Added ganged axis and squaring code
    - Next step is to merge with master and make RMT optional

--- Grbl_Esp32/cpu_map.h
+++ Grbl_Esp32/cpu_map.h
@@ -43,13 +43,17 @@
     // won't affect operation except that there will be no output
 		// form the pins. Grbl will virtually move the axis. This could 
 		// be handy if you are using a servo, etc. for another axis.
-		#define X_STEP_PIN      GPIO_NUM_12
-		#define Y_STEP_PIN      GPIO_NUM_14
-    #define Z_STEP_PIN      GPIO_NUM_27		
+		#define X_STEP_PIN      	GPIO_NUM_12
+		#define X_DIRECTION_PIN  	GPIO_NUM_26
+		#define X_RMT_CHANNEL		0
 		
-		#define X_DIRECTION_PIN   GPIO_NUM_26
-		#define Y_DIRECTION_PIN   GPIO_NUM_25  
-		#define Z_DIRECTION_PIN   GPIO_NUM_33 
+		#define Y_STEP_PIN      	GPIO_NUM_14
+		#define Y_DIRECTION_PIN   	GPIO_NUM_25
+		#define Y_RMT_CHANNEL		1
+		
+		#define Z_STEP_PIN      	GPIO_NUM_27		
+		#define Z_DIRECTION_PIN   	GPIO_NUM_33 		 
+		#define Z_RMT_CHANNEL		2		
 		
 		// OK to comment out to use pin for other features
 		#define STEPPERS_DISABLE_PIN GPIO_NUM_13		
@@ -110,10 +114,13 @@
 		//#define PEN_LASER_V1
 		#define PEN_LASER_V2
 
-		#define X_STEP_PIN      GPIO_NUM_12
-		#define Y_STEP_PIN      GPIO_NUM_14		
-		#define X_DIRECTION_PIN   GPIO_NUM_26
-		#define Y_DIRECTION_PIN   GPIO_NUM_25  
+		#define X_STEP_PIN      	GPIO_NUM_12
+		#define X_DIRECTION_PIN   	GPIO_NUM_26
+		#define X_RMT_CHANNEL		0
+		
+		#define Y_STEP_PIN      	GPIO_NUM_14
+		#define Y_DIRECTION_PIN   	GPIO_NUM_25
+		#define Y_RMT_CHANNEL		1
 		
 		#define STEPPERS_DISABLE_PIN GPIO_NUM_13
 		
@@ -203,10 +210,14 @@
 #endif
 
 #ifdef CPU_MAP_MIDTBOT  // Buildlog.net midtbot
-		#define X_STEP_PIN      GPIO_NUM_12
-		#define Y_STEP_PIN      GPIO_NUM_14		
-		#define X_DIRECTION_PIN   GPIO_NUM_26
-		#define Y_DIRECTION_PIN   GPIO_NUM_25  
+
+		#define X_STEP_PIN      	GPIO_NUM_12
+		#define X_DIRECTION_PIN   	GPIO_NUM_26
+		#define X_RMT_CHANNEL		0
+		
+		#define Y_STEP_PIN      	GPIO_NUM_14				
+		#define Y_DIRECTION_PIN   	GPIO_NUM_25  
+		#define Y_RMT_CHANNEL		1
 		
 		#ifndef COREXY // maybe set in config.h
 			#define COREXY
@@ -320,10 +331,13 @@
 #endif
 
 #ifdef CPU_MAP_POLAR_COASTER  // The Buildlog.net pen polar coaster controller V1
-		#define X_STEP_PIN      GPIO_NUM_15
-		#define Y_STEP_PIN      GPIO_NUM_2
-		#define X_DIRECTION_PIN   GPIO_NUM_25
-		#define Y_DIRECTION_PIN   GPIO_NUM_26
+		#define X_STEP_PIN      	GPIO_NUM_15
+		#define X_DIRECTION_PIN   	GPIO_NUM_25
+		#define X_RMT_CHANNEL		0
+		
+		#define Y_STEP_PIN      	GPIO_NUM_2		
+		#define Y_DIRECTION_PIN   	GPIO_NUM_26
+		#define Y_RMT_CHANNEL		1
 		
 		#define STEPPERS_DISABLE_PIN GPIO_NUM_17
 		
@@ -430,9 +444,9 @@
 		//#define PEN_LASER_V1
 		#define PEN_LASER_V2
 
-		#define X_STEP_PIN      GPIO_NUM_12
-		//#define Y_STEP_PIN      GPIO_NUM_14		
-		#define X_DIRECTION_PIN   GPIO_NUM_26
+		#define X_STEP_PIN      	GPIO_NUM_12
+		#define X_DIRECTION_PIN   	GPIO_NUM_26
+		#define X_RMT_CHANNEL		0
 		//#define Y_DIRECTION_PIN   GPIO_NUM_25  
 		
 		#define STEPPERS_DISABLE_PIN GPIO_NUM_13
@@ -535,8 +549,9 @@
 #ifdef CPU_MAP_SM // String art machine definition
 	
 
-		#define X_STEP_PIN      GPIO_NUM_12
-		#define X_DIRECTION_PIN   GPIO_NUM_26
+		#define X_STEP_PIN      	GPIO_NUM_12
+		#define X_DIRECTION_PIN   	GPIO_NUM_26
+		#define X_RMT_CHANNEL		0
 		
 		#define STEPPERS_DISABLE_PIN GPIO_NUM_13		
 		
@@ -668,19 +683,23 @@
 	
 		#define USE_GANGED_AXES // allow two motors on an axis 
 	  
-		#define X_STEP_PIN      GPIO_NUM_12
-		#define X_STEP_B_PIN    GPIO_NUM_22	 // ganged motor
+		#define X_STEP_PIN      	GPIO_NUM_12
+		#define X_RMT_CHANNEL		0
+		#define X_STEP_B_PIN    	GPIO_NUM_22	 // ganged motor
+		#define X_B_RMT_CHANNEL		1
+		#define X_DIRECTION_PIN   	GPIO_NUM_26
 		#define X_AXIS_SQUARING
 		
-		#define Y_STEP_PIN      GPIO_NUM_14
-		#define Y_STEP_B_PIN    GPIO_NUM_21  // ganged motor
+		#define Y_STEP_PIN      	GPIO_NUM_14
+		#define Y_RMT_CHANNEL		2
+		#define Y_STEP_B_PIN   		GPIO_NUM_21  // ganged motor
+		#define Y_B_RMT_CHANNEL		3		
+		#define Y_DIRECTION_PIN   	GPIO_NUM_25
 		#define Y_AXIS_SQUARING
 		
-    #define Z_STEP_PIN      GPIO_NUM_27
-		
-		#define X_DIRECTION_PIN   GPIO_NUM_26
-		#define Y_DIRECTION_PIN   GPIO_NUM_25  
-		#define Z_DIRECTION_PIN   GPIO_NUM_33 
+		#define Z_STEP_PIN      	GPIO_NUM_27		
+		#define Z_RMT_CHANNEL		4
+		#define Z_DIRECTION_PIN   	GPIO_NUM_33 
 		
 		// OK to comment out to use pin for other features
 		#define STEPPERS_DISABLE_PIN GPIO_NUM_13		
@@ -738,82 +757,7 @@
 		
 #endif
 
-#ifdef CPU_MAP_LOWRIDER
-	// This is the CPU Map for the Buildlog.net MPCNC controller
-	// used in lowrider mode. Low rider has (2) Z and one each of X and Y
-	// These will not match the silkscreen or schematic descriptions	
-	
-		#define USE_GANGED_AXES // allow two motors on an axis 
-	  
-		#define X_STEP_PIN      GPIO_NUM_27    // use Z labeled connector
-		
-		#define Y_STEP_PIN      GPIO_NUM_14
-		#define Y_STEP_B_PIN    GPIO_NUM_21  // ganged motor
-		#define Y_AXIS_SQUARING
-		
-    #define Z_STEP_PIN      GPIO_NUM_12
-		#define Z_STEP_B_PIN    GPIO_NUM_22
-		#define Y_AXIS_SQUARING
-		
-		#define X_DIRECTION_PIN   GPIO_NUM_33 // use Z labeled connector
-		#define Y_DIRECTION_PIN   GPIO_NUM_25  
-		#define Z_DIRECTION_PIN   GPIO_NUM_26 // use X labeled connector
-		
-		// OK to comment out to use pin for other features
-		#define STEPPERS_DISABLE_PIN GPIO_NUM_13		
-		
-				
-		// Note: if you use PWM rather than relay, you could map GPIO_NUM_17 to mist or flood 
-		//#define USE_SPINDLE_RELAY
-		
-		#ifdef USE_SPINDLE_RELAY		
-			#define SPINDLE_PWM_PIN    GPIO_NUM_17
-		#else
-			#define SPINDLE_PWM_PIN    GPIO_NUM_16
-			#define SPINDLE_ENABLE_PIN	GPIO_NUM_32
-		#endif
-		
-		#define SPINDLE_PWM_CHANNEL 0
-		// PWM Generator is based on 80,000,000 Hz counter
-		// Therefor the freq determines the resolution
-		// 80,000,000 / freq = max resolution
-		// For 5000 that is 80,000,000 / 5000 = 16000 
-		// round down to nearest bit count for SPINDLE_PWM_MAX_VALUE = 13bits (8192)
-		#define SPINDLE_PWM_BASE_FREQ 5000 // Hz
-		#define SPINDLE_PWM_BIT_PRECISION 8   // be sure to match this with SPINDLE_PWM_MAX_VALUE
-		#define SPINDLE_PWM_OFF_VALUE     0
-		#define SPINDLE_PWM_MAX_VALUE     255 // (2^SPINDLE_PWM_BIT_PRECISION)
-		
-		#ifndef SPINDLE_PWM_MIN_VALUE
-				#define SPINDLE_PWM_MIN_VALUE   1   // Must be greater than zero.
-		#endif
-		
-		#define SPINDLE_PWM_RANGE         (SPINDLE_PWM_MAX_VALUE-SPINDLE_PWM_MIN_VALUE)		
-		
-		// Note: Only uncomment this if USE_SPINDLE_RELAY is commented out.
-		// Relay can be used for Spindle or Coolant
-		//#define COOLANT_FLOOD_PIN 	GPIO_NUM_17
-		
-		#define X_LIMIT_PIN      	GPIO_NUM_15  
-		#define Y_LIMIT_PIN      	GPIO_NUM_4  
-		#define Z_LIMIT_PIN     	GPIO_NUM_2 	
-		#define LIMIT_MASK      	B111
-		
-		#define PROBE_PIN       	GPIO_NUM_35  
-		
-		// The default value in config.h is wrong for this controller
-		#ifdef INVERT_CONTROL_PIN_MASK
-			#undef INVERT_CONTROL_PIN_MASK			
-		#endif
-		
-		#define INVERT_CONTROL_PIN_MASK   B1110
-		
-		// Note: check the #define IGNORE_CONTROL_PINS is the way you want in config.h
-		#define CONTROL_RESET_PIN         GPIO_NUM_34  // needs external pullup
-		#define CONTROL_FEED_HOLD_PIN     GPIO_NUM_36  // needs external pullup 
-		#define CONTROL_CYCLE_START_PIN   GPIO_NUM_39  // needs external pullup    		
-		
-#endif
+
 
 	// ================= common to all machines ================================
 	
